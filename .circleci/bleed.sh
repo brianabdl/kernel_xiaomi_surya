e4c4af8133fe403416177d6511bdabd59edf2313
SiAlone <100053585+SiAlone@users.noreply.github.com>
2022-02-20 10:12:44 +0700

    bleed.sh: Initial script azrim adapt

--- .circleci/bleed.sh
+++ .circleci/bleed.sh
@@ -1,23 +1,20 @@
-#!/usr/bin/env bash
-echo "Cloning dependencies"
-git clone --depth=1 https://github.com/SiAlone/kernel_xiaomi_surya/ -b  v8  kernel
-cd kernel
-git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
-git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b surya AnyKernel
-git clone --depth=1 https://android.googlesource.com/platform/system/libufdt libufdt
-echo "Done"
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-TANGGAL=$(date +"%F-%S")
-START=$(date +"%s")
-export CONFIG_PATH=$PWD/arch/arm64/configs/surya-perf_defconfig
-PATH="${PWD}/clang/bin:$PATH"
-export ARCH=arm64
-export KBUILD_BUILD_HOST=circleci
-export KBUILD_BUILD_USER="SiAlone"
+#!/bin/bash
+#
+# Copyright (C) 2020 azrim.
+# All rights reserved.
 
-# Telegram
-chat_id="-1001786450765"
-token="5136571256:AAEVb6wcnHbB358erxRQsP4crhW7zNh_7p8"
+# Init
+KERNEL_DIR="${PWD}"
+cd "$KERNEL_DIR" || exit
+DTB_TYPE="single" # define as "single" if want use single file
+KERN_IMG=/root/project/kernel_xiaomi_surya/out/arch/arm64/boot/Image.gz-dtb   # if use single file define as Image.gz-dtb instead
+# KERN_DTB="${KERNEL_DIR}"/out/arch/arm64/boot/dtbo.img       # and comment this variable
+ANYKERNEL="${HOME}"/anykernel
+LOGS="${HOME}"/${CHEAD}.log
+
+# Repo URL
+ANYKERNEL_REPO="https://github.com/dekukamikix/anykernel3.git"
+ANYKERNEL_BRANCH="master"
 
 # Repo info
 PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
@@ -27,91 +24,176 @@ CHEAD="$(git rev-parse --short HEAD)"
 LATEST_COMMIT="[$COMMIT_POINT](https://github.com/SiAlone/kernel_xiaomi_surya/commit/$CHEAD)"
 LOGS_URL="[See Circle CI Build Logs Here](https://circleci.com/gh/SiAlone/kernel_xiaomi_surya/$CIRCLE_BUILD_NUM)"
 
+# Compiler
+mkdir -p "/mnt/workdir/proton-clang"
+COMP_TYPE="clang" # unset if want to use gcc as compiler
+CLANG_DIR="/mnt/workdir/proton-clang"
+CLANG_URL="https://github.com/kdrag0n/proton-clang"
+GCC_DIR="" # Doesn't needed if use proton-clang
+GCC32_DIR="" # Doesn't needed if use proton-clang
+CLANG_FILE="/mnt/workdir/clang.tar.gz"
+
+git clone https://github.com/kdrag0n/proton-clang.git --depth=1 --single-branch $CLANG_DIR -b master
+
+if [[ "${COMP_TYPE}" =~ "clang" ]]; then
+    CSTRING=$("$CLANG_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+    COMP_PATH="$CLANG_DIR/bin:${PATH}"
+else
+    COMP_PATH="${GCC_DIR}/bin:${GCC32_DIR}/bin:${PATH}"
+fi
+
+# Defconfig
+DEFCONFIG="vendor/surya-perf_defconfig"
+REGENERATE_DEFCONFIG="" # unset if don't want to regenerate defconfig
+
+# Telegram
+CHATID="-1001786450765" # Group/channel chatid (use rose/userbot to get it)
+TELEGRAM_TOKEN="5136571256:AAEVb6wcnHbB358erxRQsP4crhW7zNh_7p8"
+
+# Export Telegram.sh
+TELEGRAM_FOLDER="${HOME}"/telegram
+if ! [ -d "${TELEGRAM_FOLDER}" ]; then
+    git clone https://github.com/fabianonline/telegram.sh/ "${TELEGRAM_FOLDER}"
+fi
+
+TELEGRAM="${TELEGRAM_FOLDER}"/telegram
+tg_cast() {
+	curl -s -X POST https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendMessage -d disable_web_page_preview="true" -d chat_id="$CHATID" -d "parse_mode=MARKDOWN" -d text="$(
+		for POST in "${@}"; do
+			echo "${POST}"
+		done
+	)" &> /dev/null
+}
+tg_ship() {
+    "${TELEGRAM}" -f "${ZIPNAME}" -t "${TELEGRAM_TOKEN}" -c "${CHATID}" -H \
+    "$(
+                for POST in "${@}"; do
+                        echo "${POST}"
+                done
+    )"
+}
+tg_fail() {
+    "${TELEGRAM}" -f "${LOGS}" -t "${TELEGRAM_TOKEN}" -c "${CHATID}" -H \
+    "$(
+                for POST in "${@}"; do
+                        echo "${POST}"
+                done
+    )"
+}
+
+# Versioning
+versioning() {
+    cat arch/arm64/configs/"${DEFCONFIG}" | grep CONFIG_LOCALVERSION= | tee /mnt/workdir/name.sh
+    sed -i 's/-${KERNEL}-//g' /mnt/workdir/name.sh
+    source /mnt/workdir/name.sh
+}
+
 # Costumize
 versioning
-KERNEL="beta-Quantumn"
+KERNEL="StormBreaker-Test"
 DEVICE="Surya"
 KERNELTYPE="rev.0.1"
 KERNELNAME="${KERNEL}-${DEVICE}-${KERNELTYPE}-$(date +%y%m%d-%H%M)"
 TEMPZIPNAME="${KERNELNAME}-unsigned.zip"
 ZIPNAME="${KERNELNAME}.zip"
 
-# sticker plox
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAADBQADVAADaEQ4KS3kDsr-OWAUFgQ" \
-        -d chat_id=$chat_id
+# Regenerating Defconfig
+regenerate() {
+    cp out/.config arch/arm64/configs/"${DEFCONFIG}"
+    git add arch/arm64/configs/"${DEFCONFIG}"
+    git commit -m "defconfig: Regenerate"
 }
-function tg_cast() {
-	curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d disable_web_page_preview="true" \
-        -d chat_id="$chat_id" \
-        -d "parse_mode=MARKDOWN" \
-        -d text="*CI Build $CIRCLE_BUILD_NUM Triggered*" \
-	            "Compiling with *$(nproc --all)* CPUs" \
-	            "-----------------------------------------" \
-	            "*Compiler ver:* ${CSTRING}" \
-            	"*Device:* ${DEVICE}" \
-            	"*Kernel name:* ${KERNEL}" \
-            	"*Build ver:* ${KERNELTYPE}" \
-            	"*Linux version:* $(make kernelversion)" \
-            	"*Branch:* ${CIRCLE_BRANCH}" \
-            	"*Clocked at:* ${NOW}" \
-            	"*Latest commit:* ${LATEST_COMMIT}" \
-             	"------------------------------------------" \
-	            "${LOGS_URL}"
+
+# Build Failed
+build_failed() {
+	    END=$(date +"%s")
+	    DIFF=$(( END - START ))
+	    echo -e "Kernel compilation failed, See build log to fix errors"
+	    tg_fail "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)!"
+	    exit 1
 }
-# Send info plox channel
-function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>• surya-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Poco x3</b> (RMX1851)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
+
+# Building
+makekernel() {
+    sed -i "s/${KERNELTYPE}/${KERNELTYPE}/g" "${KERNEL_DIR}/arch/arm64/configs/${DEFCONFIG}"
+    echo "SiAlone@MacBook-Pro-2012" > "$KERNEL_DIR"/.builderdata
+    export PATH="${COMP_PATH}"
+    make O=out ARCH=arm64 ${DEFCONFIG} savedefconfig
+    if [[ "${REGENERATE_DEFCONFIG}" =~ "true" ]]; then
+        regenerate
+    fi
+    if [[ "${COMP_TYPE}" =~ "clang" ]]; then
+        make -j$(nproc --all) CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- O=out ARCH=arm64 LLVM=1 2>&1 LD=ld.lld | tee "$LOGS"
+    else
+      	make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE="${GCC_DIR}/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${GCC32_DIR}/bin/arm-eabi-"
+    fi
+    # Check If compilation is success
+    packingkernel
 }
-# Push kernel to channel
-function push() {
-    cd AnyKernel
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Poco x3 (surya)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+
+# Packing kranul
+packingkernel() {
+    # Copy compiled kernel
+    if [ -d "${ANYKERNEL}" ]; then
+        rm -rf "${ANYKERNEL}"
+    fi
+    git clone "$ANYKERNEL_REPO" -b "$ANYKERNEL_BRANCH" "${ANYKERNEL}"
+    if ! [ -f /root/project/kernel_xiaomi_surya/out/arch/arm64/boot/Image.gz-dtb ]; then
+        build_failed
+    fi
+    cp /root/project/kernel_xiaomi_surya/out/arch/arm64/boot/Image.gz-dtb /root/anykernel/Image.gz-dtb
+    cp /root/project/kernel_xiaomi_surya/out/arch/arm64/boot/dtbo.img /root/anykernel/dtbo.img
+    : 'if ! [ -f "${KERN_IMG}" ]; then
+        build_failed
+    fi
+    if ! [ -f "${KERN_DTB}" ]; then
+        build_failed
+    fi
+    if [[ "${DTB_TYPE}" =~ "single" ]]; then
+        cp "${KERN_IMG}" "${ANYKERNEL}"/Image.gz-dtb
+    else
+        cp "${KERN_IMG}" "${ANYKERNEL}"/Image.gz-dtb
+        cp "${KERN_DTB}" "${ANYKERNEL}"/dtbo.img
+    fi
+    '
+
+    # Zip the kernel, or fail
+    cd "${ANYKERNEL}" || exit
+    zip -r9 "${TEMPZIPNAME}" ./*
+
+    # Sign the zip before sending it to Telegram
+    curl -sLo zipsigner-4.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel3/master/zipsigner-4.0.jar
+    java -jar zipsigner-4.0.jar "${TEMPZIPNAME}" "${ZIPNAME}"
+
+    END=$(date +"%s")
+    DIFF=$(( END - START ))
+
+    # Ship it to the CI channel
+    tg_ship "<b>-------- Build #$CIRCLE_BUILD_NUM Succeeded --------</b>" \
+            "" \
+            "<b>Device:</b> ${DEVICE}" \
+            "<b>Build rev:</b> ${KERNELTYPE}" \
+            "<b>HEAD Commit:</b> ${CHEAD}" \
+            "<b>Time elapsed:</b> $((DIFF / 60)):$((DIFF % 60))" \
+            "" \
+            "Try it and give me some thoughts!"
 }
-# Fin Error
-function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Build threw an error(s)"
-    exit 1
-}
-# Compile plox
-function compile() {
-   make O=out ARCH=arm64 surya-perf_defconfig
-       make -j$(nproc --all) O=out \
-                             ARCH=arm64 \
-			     CC=clang \
-			     CROSS_COMPILE=aarch64-linux-gnu- \
-			     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-   python2 "libufdt/utils/src/mkdtboimg.py" \
-					create "out/arch/arm64/boot/dtbo.img" --page_size=4096 out/arch/arm64/boot/dts/qcom/*.dtbo
-   cp out/arch/arm64/boot/dtbo.img AnyKernel
-}
-# Zipping
-function zipping() {
-    cd AnyKernel || exit 1
-    zip -r9 ${ZIPNAME}.zip *
-    cd .. 
-}
-sticker
-tg_cast
-sendinfo
-compile
-zipping
-END=$(date +"%s")
-DIFF=$(($END - $START))
-push
+
+# Starting
+NOW=$(date +%d/%m/%Y-%H:%M)
+START=$(date +"%s")
+tg_cast "*CI Build #$CIRCLE_BUILD_NUM Triggered*" \
+	"Compiling with *$(nproc --all)* CPUs" \
+	"-----------------------------------------" \
+	"*Compiler ver:* ${CSTRING}" \
+	"*Device:* ${DEVICE}" \
+	"*Kernel name:* ${KERNEL}" \
+	"*Build ver:* ${KERNELTYPE}" \
+	"*Linux version:* $(make kernelversion)" \
+	"*Branch:* ${CIRCLE_BRANCH}" \
+	"*Clocked at:* ${NOW}" \
+	"*Latest commit:* ${LATEST_COMMIT}" \
+ 	"------------------------------------------" \
+	"${LOGS_URL}"
+
+makekernel
