09d84f1979b452f2c2ffe5ed66edc098accbb2dc
SiAlone <100053585+SiAlone@users.noreply.github.com>
2022-02-20 10:38:22 +0700

    bleed.sh: regenerate defconfig true

--- .circleci/bleed.sh
+++ .circleci/bleed.sh
@@ -44,7 +44,7 @@ fi
 
 # Defconfig
 DEFCONFIG="vendor/surya-perf_defconfig"
-REGENERATE_DEFCONFIG="" # unset if don't want to regenerate defconfig
+REGENERATE_DEFCONFIG="true" # unset if don't want to regenerate defconfig
 
 # Telegram
 CHATID="-1001786450765" # Group/channel chatid (use rose/userbot to get it)
