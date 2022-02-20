f9e25fda06129a2bba16b1acf94207a8dbe54145
SiAlone <100053585+SiAlone@users.noreply.github.com>
2022-02-20 11:47:54 +0700

    Update bleed.sh

--- .circleci/bleed.sh
+++ .circleci/bleed.sh
@@ -29,32 +29,6 @@ token="5136571256:AAEVb6wcnHbB358erxRQsP4crhW7zNh_7p8"
 CHATID="${chat_id}"
 TELEGRAM_TOKEN="${token}"
 
-# Export Telegram.sh
-TELEGRAM_FOLDER="${HOME}"/telegram
-if ! [ -d "${TELEGRAM_FOLDER}" ]; then
-    git clone https://github.com/fabianonline/telegram.sh/ "${TELEGRAM_FOLDER}"
-fi
-
-TELEGRAM="${TELEGRAM_FOLDER}"/telegram
-
-# Ship it to the CI channel
-tg_ship "<b>-------- Build #$CIRCLE_BUILD_NUM Succeeded --------</b>" \
-      "" \
-      "<b>Device:</b> ${DEVICE}" \
-      "<b>Build rev:</b> ${KERNELTYPE}" \
-      "<b>HEAD Commit:</b> ${CHEAD}" \
-      "<b>Time elapsed:</b> $((DIFF / 60)):$((DIFF % 60))" \
-      "" \
-      "Try it and give me some thoughts!"
-
-# Build Failed
-build_failed() {
-	    END=$(date +"%s")
-	    DIFF=$(( END - START ))
-	    echo -e "Kernel compilation failed, See build log to fix errors"
-	    tg_fail "Build for ${DEVICE} <b>failed</b> in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)!"
-	    exit 1
-}
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
@@ -66,16 +40,20 @@ function sendinfo() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>• surya-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Poco x3</b> (RMX1851)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
-}
-tg_ship() {
-    "${TELEGRAM}" -f "${ZIPNAME}" -t "${TELEGRAM_TOKEN}" -c "${CHATID}" -H \
-    "$(
-                for POST in "${@}"; do
-                        echo "${POST}"
-                done
-    )"
+        -d "parse_mode=MARKDOWN" \
+        -d text="*CI Build #$CIRCLE_BUILD_NUM Triggered*" \
+	"Compiling with *$(nproc --all)* CPUs" \
+	"-----------------------------------------" \
+	"*Compiler ver:* ${CSTRING}" \
+	"*Device:* ${DEVICE}" \
+	"*Kernel name:* ${KERNEL}" \
+	"*Build ver:* ${KERNELTYPE}" \
+	"*Linux version:* $(make kernelversion)" \
+	"*Branch:* ${CIRCLE_BRANCH}" \
+	"*Clocked at:* ${NOW}" \
+	"*Latest commit:* ${LATEST_COMMIT}" \
+ 	"------------------------------------------" \
+	"${LOGS_URL}"
 }
 # Push kernel to channel
 function push() {
@@ -87,13 +65,6 @@ function push() {
         -F "parse_mode=html" \
         -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Poco x3 (surya)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 }
-tg_cast() {
-	curl -s -X POST https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendMessage -d disable_web_page_preview="true" -d chat_id="$CHATID" -d "parse_mode=MARKDOWN" -d text="$(
-		for POST in "${@}"; do
-			echo "${POST}"
-		done
-	)" &> /dev/null
-}
 # Fin Error
 function finerr() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
@@ -102,14 +73,6 @@ function finerr() {
         -d "parse_mode=markdown" \
         -d text="Build threw an error(s)"
     exit 1
-}
-tg_fail() {
-    "${TELEGRAM}" -f "${LOGS}" -t "${TELEGRAM_TOKEN}" -c "${CHATID}" -H \
-    "$(
-                for POST in "${@}"; do
-                        echo "${POST}"
-                done
-    )"
 }
 # Compile plox
 function compile() {
@@ -127,26 +90,9 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 surya-Stormbreaker-${TANGGAL}.zip *
+    zip -r9 ${ZIPNAME}.zip *
     cd .. 
 }
-# Starting
-NOW=$(date +%d/%m/%Y-%H:%M)
-START=$(date +"%s")
-tg_cast "*CI Build #$CIRCLE_BUILD_NUM Triggered*" \
-	"Compiling with *$(nproc --all)* CPUs" \
-	"-----------------------------------------" \
-	"*Compiler ver:* ${CSTRING}" \
-	"*Device:* ${DEVICE}" \
-	"*Kernel name:* ${KERNEL}" \
-	"*Build ver:* ${KERNELTYPE}" \
-	"*Linux version:* $(make kernelversion)" \
-	"*Branch:* ${CIRCLE_BRANCH}" \
-	"*Clocked at:* ${NOW}" \
-	"*Latest commit:* ${LATEST_COMMIT}" \
- 	"------------------------------------------" \
-	"${LOGS_URL}"
-
 sticker
 sendinfo
 compile
