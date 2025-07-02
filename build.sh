ee8d5196f07a5929f3a35693972fafd7d49ef743
brianabdl <mohammadbrianabdillah@gmail.com>
2025-07-02 15:43:46 +0000

    build.sh: always update AnyKernel3

--- build.sh
+++ build.sh
@@ -97,9 +97,10 @@ cd - || {
 
 if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
 	echo -e "\nKernel compiled succesfully! Zipping up...\n"
-	if [ -d "$AK3_DIR" ]; then
-		cp -r $AK3_DIR AnyKernel3
-	elif ! git clone -q https://github.com/surya-aosp/AnyKernel3 -b shinigami; then
+	if [ -d "AnyKernel3" ]; then
+		rm -rf AnyKernel3
+	fi
+	if ! git clone -q https://github.com/surya-aosp/AnyKernel3 -b shinigami; then
 		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 		exit 1
 	fi
